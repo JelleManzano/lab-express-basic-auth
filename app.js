5afcb57fc5b59ad3f3fc06b92fd6b62506b5734c
Jelle <jmanzanohaagsma@gmail.com>
2022-10-19 23:54:59 +0200

    Fixed a require in the app to show the sessions

--- app.js
+++ app.js
@@ -30,6 +30,17 @@ app.use('/', index);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
+app.use((req,res,next) => {
+   
+    if(req.session.activeUser === undefined) {
+      
+        res.locals.isUserActive = false
+    } else {
+        
+        res.locals.isUserActive = true
+    }
+    next()
+})
 
 module.exports = app;
 
